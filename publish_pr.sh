b2146eedba958f3ac63a52257d63f6bdfd1f4ad7
Sol-Raksha Raja <raj@cambridgespark.com>
2022-12-13 12:01:33 +0000

    Add error handler

--- publish_pr.sh
+++ publish_pr.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+function handle_error() {
+    notify-send -i info "Something went wrong"
+}
 source /home/r/workspace/lazygit/ENV
 source /home/r/workspace/lazygit/venv/bin/activate
 DEFAULT_BRANCH=$(git remote show origin | grep "HEAD branch" | sed 's/.*: //')
 REPO_NAME=$(git remote -v | grep '(push)' | cut -d ":" -f 2 | cut -d "." -f 1)
 SOURCE_BRANCH=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
 TITLE=$(git log -1 --pretty=%s)
-git push -u origin "$SOURCE_BRANCH"
-python /home/r/workspace/lazygit/lazy_push_handler.py "$REPO_NAME" --source "$SOURCE_BRANCH" --target "$DEFAULT_BRANCH" --message "$TITLE" "$@"
+git push -u origin "$SOURCE_BRANCH" || handle_error
+python /home/r/workspace/lazygit/lazy_push_handler.py "$REPO_NAME" --source "$SOURCE_BRANCH" --target "$DEFAULT_BRANCH" --message "$TITLE" "$@" || handle_error
